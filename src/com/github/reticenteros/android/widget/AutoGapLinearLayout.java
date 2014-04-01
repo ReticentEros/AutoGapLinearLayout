0153f1983c95ce0ba2274c3ad924da606b86469a
ReticentEros <ReticentEros@gmail.com>
2014-04-01 20:27:08 +0800

    Append Functionality First Gap to AutoGapLinearLayout
    Append method setFirstGap and relatives to AutoGapLinearLayout.
    Let AutoGapLinearLayout auto-add the first gap on instantiation/reset.
    
    - Append private methods generateGap and generateGapLayoutParams
      to help generate gaps.
    
    - Append public method setFirstGap for setting/resetting the first gap.
    
    - Append XML attr firstGap and modify public constructors.
      Let AutoGapLinearLayout auto-add the first gap on instantiation.
    
    - Append and override public methods removeAllViews.
      Let AutoGapLinearLayout auto-add the first gap on reset.
    
    - Each width of gaps when horizontal orientation,
      or each height of gaps when vertical orientation
      is controlled by a variable, LinearLayout.LayoutParams.weight.

--- src/com/github/reticenteros/android/widget/AutoGapLinearLayout.java
+++ src/com/github/reticenteros/android/widget/AutoGapLinearLayout.java
@@ -1,9 +1,13 @@
 package com.github.reticenteros.android.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Space;
+import com.github.reticenteros.android.R;
 
 public class AutoGapLinearLayout extends LinearLayout {
     /* Constants */
@@ -14,6 +18,7 @@ public class AutoGapLinearLayout extends LinearLayout {
     public AutoGapLinearLayout(Context context) {
         super(context);
         if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "AutoGapLinearLayout(Context)");
+        setFirstGap(1);
     }
 
     public AutoGapLinearLayout(Context context, AttributeSet attrs) {
@@ -23,6 +28,61 @@ public class AutoGapLinearLayout extends LinearLayout {
 
     public AutoGapLinearLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+
         if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "AutoGapLinearLayout(Context, AttributeSet, int)");
+
+        TypedArray a = context.obtainStyledAttributes(
+                attrs,
+                R.styleable.AutoGapLinearLayout,
+                defStyle,
+                0);
+
+        try {
+            setFirstGap(a.getFloat(R.styleable.AutoGapLinearLayout_firstGap, 1));
+        } finally {
+            a.recycle();
+        }
+    }
+
+    /* Inherited Methods */
+    @Override
+    public void removeAllViews() {
+        if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "removeAllViews()");
+        super.removeAllViews();
+        setFirstGap(1);
+    }
+
+    /* Public Methods */
+    public void removeAllViews(float firstGapWeight) {
+        if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "removeAllViews(float)");
+        super.removeAllViews();
+        setFirstGap(firstGapWeight);
+    }
+
+    public void setFirstGap(float weight) {
+        if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "setFirstGap(float)");
+
+        View firstGap = getChildAt(0);
+        LayoutParams params = generateGapLayoutParams(weight);
+
+        if (firstGap == null) super.addView(generateGap(), 0, params);
+        else firstGap.setLayoutParams(params);
+    }
+
+    /* Private Methods */
+    private Space generateGap() {
+        if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "generateGap()");
+        return new Space(getContext());
+    }
+
+    private LayoutParams generateGapLayoutParams(float weight) {
+        if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "generateGapLayoutParams(float)");
+
+        int orientation = getOrientation();
+
+        if (orientation == HORIZONTAL) return new LayoutParams(0, LayoutParams.MATCH_PARENT, weight);
+        else if (orientation == VERTICAL) return new LayoutParams(LayoutParams.MATCH_PARENT, 0, weight);
+
+        return null;
     }
 }
