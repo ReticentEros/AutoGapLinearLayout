00033c12f19c92b1bcebd0b237fb7cfad91b4cc0
ReticentEros <ReticentEros@gmail.com>
2014-04-02 18:55:17 +0800

    Modify Functionality Add View in AutoGapLinearLayout
    Modify public method addView and relatives in AutoGapLinearLayout.
    Let AutoGapLinearLayout auto-add gap after view added.
    
    - Override nested class LayoutParams with new layout param., gap,
      to control gap afterward.
    
    - Append XML attr layout_gap for XML layout support.
    
    - Override public method generateLayoutParams for system call.
    
    - Override protected methods checkLayoutParams,
      generateDefaultLayoutParams and generateLayoutParams for invocation.
    
    - Override public method addView to add view with a gap.
    
    - Each gap is tagged with relative view.

--- src/com/github/reticenteros/android/widget/AutoGapLinearLayout.java
+++ src/com/github/reticenteros/android/widget/AutoGapLinearLayout.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.Space;
 import com.github.reticenteros.android.R;
@@ -46,10 +47,59 @@ public class AutoGapLinearLayout extends LinearLayout {
 
     /* Inherited Methods */
     @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "addView(View, int, ViewGroup.LayoutParams)");
+
+        if (index >= 0 && index % 2 == 0) {
+            if (LOG_PRIORITY <= Log.WARN) {
+                Log.w(LOG_TAG, "addView(View, int, ViewGroup.LayoutParams) does nothing since indicated a gap index");
+            }
+
+            return;
+        } else {
+            super.addView(child, index, params);
+
+            Space gap = generateGap();
+            gap.setTag(child);
+
+            if (index != -1) index++;
+
+            float weight = 1;
+            if (checkLayoutParams(params)) weight = ((LayoutParams) params).gap;
+
+            super.addView(gap, index, generateGapLayoutParams(weight));
+        }
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "generateLayoutParams(AttributeSet)");
+        return new LayoutParams(getContext(), attrs);
+    }
+
+    @Override
     public void removeAllViews() {
         if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "removeAllViews()");
         super.removeAllViews();
         setFirstGap(1);
+    }
+
+    @Override
+    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
+        if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "checkLayoutParams(ViewGroup.LayoutParams)");
+        return p instanceof LayoutParams;
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "generateDefaultLayoutParams()");
+        return new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "generateLayoutParams(ViewGroup.LayoutParams)");
+        return new LayoutParams(p);
     }
 
     /* Public Methods */
@@ -84,5 +134,55 @@ public class AutoGapLinearLayout extends LinearLayout {
         else if (orientation == VERTICAL) return new LayoutParams(LayoutParams.MATCH_PARENT, 0, weight);
 
         return null;
+    }
+
+    /* Nested Classes */
+    public static class LayoutParams extends LinearLayout.LayoutParams {
+        /* Constants */
+        private static final String LOG_TAG = "widget.AutoGapLinearLayout.LayoutParams";
+
+        /* Fields */
+        public float gap;
+
+        /* Public Constructors */
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+
+            if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "LayoutParams(Context, AttributeSet)");
+
+            TypedArray a = c.obtainStyledAttributes(
+                    attrs,
+                    R.styleable.AutoGapLinearLayout_LayoutParams);
+
+            try {
+                gap = a.getFloat(R.styleable.AutoGapLinearLayout_LayoutParams_layout_gap, 1);
+            } finally {
+                a.recycle();
+            }
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+            if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "LayoutParams(int, int)");
+            gap = 1;
+        }
+
+        public LayoutParams(int width, int height, float weight) {
+            super(width, height, weight);
+            if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "LayoutParams(int, int, float)");
+            gap = 1;
+        }
+
+        public LayoutParams(int width, int height, float weight, float gap) {
+            super(width, height, weight);
+            if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "LayoutParams(int, int, float, float)");
+            this.gap = gap;
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
+            if (LOG_PRIORITY <= Log.VERBOSE) Log.v(LOG_TAG, "LayoutParams(ViewGroup.LayoutParams)");
+            gap = 1;
+        }
     }
 }
